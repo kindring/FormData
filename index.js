e339c36fca5efce4c7c716ac258caac264f14de1
kindring <2460131004@qq.com>
2023-10-12 11:40:56 +0800

    依赖导入更新

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
  */
 
 
-import FormItem from "./lib/formData";
+import Form from "./lib/formData";
 
 
-import FieldCheck from "./lib/fieldCheck";
-export const FormItem = FormItem;
-export const FieldCheck = FieldCheck;
+import F from "./lib/fieldCheck";
+export const FormItem = Form;
+export const FieldCheck = F;
 // 示例
 /**
  * @example
